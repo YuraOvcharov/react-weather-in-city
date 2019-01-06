a84f7e5cd5c1e37ffe38586b66d3a4b9b54adb0b
YuraOvcharov <yuraovcharov13@gmail.com>
2019-01-06 21:47:33 +0300

    change gitignore

---
+++ build/precache-manifest.57972d91d2ce508b0ea2b74e3680d39e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a43a3c4809dfb15ce5ff140a0299df75",
+    "url": "/static/media/icon-celsius.a43a3c48.svg"
+  },
+  {
+    "revision": "ddc8a3e1d18f5f8d9548693dbb2570e3",
+    "url": "/static/media/background-img.ddc8a3e1.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10efa9498584d9498126",
+    "url": "/static/js/main.10efa949.chunk.js"
+  },
+  {
+    "revision": "3ff765798ab3a264060a",
+    "url": "/static/js/1.3ff76579.chunk.js"
+  },
+  {
+    "revision": "10efa9498584d9498126",
+    "url": "/static/css/main.0732f754.chunk.css"
+  },
+  {
+    "revision": "3ff765798ab3a264060a",
+    "url": "/static/css/1.af3ca440.chunk.css"
+  },
+  {
+    "revision": "5bb34628825cfed1e933b2966662a5e8",
+    "url": "/index.html"
+  }
+];
